397fe3955f5bdb9360a1f68ea171f3790ff4eb6a
WTFMANNNNNNNNNNNNNNNNNNNNNN <mnguy187@eq.edu.au>
2025-12-18 00:59:16 +1000

    Update growth.js
    increased size of tonk

--- server/Game/gamemodeconfigs/growth.js
+++ server/Game/gamemodeconfigs/growth.js
@@ -5,7 +5,7 @@ module.exports = {
         if (level <= 40) return 1;
         if (level <= 45 && (level & 1) == 1) return 1;
         if (level <= 51 && (level % 2) == 1) return 1;
-        if (level % 10 == 1) return 1;
+        if (level % 18 == 1) return 1;
         return 0;
     },
 };
